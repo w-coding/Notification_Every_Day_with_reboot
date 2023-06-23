991cc96e1add6540e8929ad627501e2c145ef003
Walla coding <122129717+w-coding@users.noreply.github.com>
2023-06-24 00:03:33 +0300

    main

--- main/java/com/dya/notifi/BootReceiver.java
+++ main/java/com/dya/notifi/BootReceiver.java
@@ -13,27 +13,15 @@ public class BootReceiver extends BroadcastReceiver {
             // Reschedule all notifications
             ScheduleNotification.scheduleNotification(context, 1, 0, 20,
                     "TitleTest", "MessageTest // MessageTest");
-
-
-        }
-
-
-       /* if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+             /* 
             // Reschedule all notifications
             ScheduleNotification.scheduleNotification(context, 2, 22, 59,
                     "ویردەکانی بەیانیان", "ئیستا کاتی خویندنی ویردەکانی بەیانیانە ☀");
+                    */
 
         }
 
-        try {
-            //la kati restart am basha code run abet w disan bangi dahatw miqat akat
 
-            ScheduleNotification.scheduleNotification(context, 2, 23, 24,
-                    "ویردەکانی بەیانیان", "ئیستا کاتی خویندنی ویردەکانی بەیانیانە ☀");
-
-        } catch (Exception e) {
-            Log.e("Dya", "", e);
-        }
-        */
+      
     }
 }
